ef50ef0dcdf7bbf8d1605fc5e1be94ec533c63b5
Aung Khant Myo <twizzy.indy@gmail.com>
2021-02-17 15:20:42 +0630

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Count People
+
+I just wanted to count on my people who protesting in Burma on Feb/2021. Photos in this repo will go credit for every owners who owned them.
+
+**Requirements**
+
+* tensorflow
+* keras
+* numpy
+* pillow
+* scipy
+* h5py
+* matplotlib
+* opencv-python 
+* keras-resnet
+* imageai
+
+**Dependency Files**
+
+You need to download yolo.h5 file before running. You can download it from
+[here](https://github.com/OlafenwaMoses/ImageAI/releases/tag/1.0)
+
+**Making python environment**
+
+```bash
+python3.8 -m venv venv
+```
+**Activating python environment**
+
+```bash
+source venv/bin/activate
+```
+
+**Installing requirements**
+
+```bash
+pip install -r requirements
+```
+
+**Usage**
+
+```bash
+python main.py -input test.jpg
+```
+
+**Screenshots**
+
---
+++ main.py
@@ -0,0 +1,54 @@
+#!/usr/local/bin/python3.8
+
+#######################################
+## author : Aung Khant Myo(TwizzyIndy)
+## date : Feb/2021
+#######################################
+
+
+from imageai.Detection import ObjectDetection
+import os
+
+def main():
+    
+    if len(os.sys.argv) < 2:
+        helpInfos()
+        return
+    
+    imagePath = ""
+
+    if os.sys.argv[1] == "-input":
+        imagePath = os.sys.argv[2]
+    
+    outputImagePath = os.path.basename(imagePath) + "_out" + os.path.splitext(imagePath)[1]
+    execution_path = os.getcwd()
+
+    detector = ObjectDetection()
+    detector.setModelTypeAsYOLOv3()
+    detector.setModelPath(os.path.join(execution_path, "yolo.h5"))
+    detector.loadModel()
+
+    custom_objects = detector.CustomObjects(person=True)
+    detections = detector.detectCustomObjectsFromImage(custom_objects=custom_objects,
+     input_image=imagePath,
+      output_image_path=outputImagePath,
+      minimum_percentage_probability=10)
+
+    for eachObject in detections:
+        print(eachObject["name"] , " : ", eachObject["percentage_probability"], " : ", eachObject["box_points"] )
+        print("--------------------------------")
+
+    print("\n\nFound person in image : " + str(len(detections)))
+
+    return
+
+def helpInfos():
+    print("")
+    print("Count People")
+    print("TwizzyIndy (Feb/2021)")
+    print("python3 main.py -input test1.jpg")
+    print("")
+    return
+
+if __name__ == "__main__":
+    main()
